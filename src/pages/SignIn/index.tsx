28c91730c45e849c1af3061fcffc679dbe8d0e46
joaofelix <joaofelix27@gmail.com>
2023-01-29 16:52:35 -0300

    refactor: changing all material ui grid to styled components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,11 +1,4 @@
-import {
-  Grid,
-  Typography,
-  Stack,
-  Link as MuiLink,
-  FormControlLabel,
-  Checkbox,
-} from "@mui/material";
+import { FormControlLabel, Checkbox } from "@mui/material";
 import { FC } from "react";
 import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
 import { TypeOf } from "zod";
@@ -16,6 +9,19 @@ import { ReactComponent as GitHubLogo } from "../../assets/github.svg";
 import { LinkItem, OauthMuiLink } from "../../shared/styled";
 import LoadingButtonMui from "../../components/LoadingButtom";
 import { loginSchema } from "../../schemas/SignIn";
+import {
+  BottomLoginContainer,
+  LeftBox,
+  LinkText,
+  LoginBorder,
+  LoginCard,
+  LoginGrid,
+  LoginInsideGrid,
+  LoginOutsideContainer,
+  LoginText,
+  RightBox,
+  TrustDeviceText,
+} from "./style";
 import styled from "styled-components";
 
 // 👇 Infer the Schema to get the TS Type
@@ -39,85 +45,37 @@ const LoginPage: FC = () => {
     console.log(values);
   };
 
-  const LoginOutsideContainer = styled.div`
-  height: 100vh;
-  background-color: #f4f4f4;
-`;
+  const LeftBoxContainer = styled.div`
+    width: 100%;
+    @media (min-width: 600px) {
+    width: 50%;
+    border-right: 1px solid #ddd;
+  }
+  `;
 
-const LoginGrid = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const LoginCard = styled.div`
-  max-width: 70rem;
-  width: 100%;
-  background-color: #fff;
-  box-shadow: 0 0 5px #ddd;
-`;
-
-const LoginBorder = styled.div`
-  box-shadow: 0 0 5px #ddd;
-  padding: 6rem 1rem;
-`;
-
-const LoginInsideGrid = styled.div`
-  display:flex;
-  justify-content:space-between;
-  row-gap: 10;
-  max-width:45rem;
-  margin-inline:auto;
-`;
-
-const LeftBox = styled.form`
-  display:flex;
-  flex-direction:column;
-  width:360px;
-  padding-right:3rem;
-`;
-
-const RightBox = styled.div`
-  display:flex;
-  flex-direction:column;
-  width:360px;
-  padding-left:3rem;
-  row-gap:1rem;
-`;
+  const RightBoxContainer = styled.div`
+    width: 100%;
+    margin-top:10px;
+    @media (min-width: 600px) {
+    width: 50%;
+  }
+  `;
 
   // 👇 JSX to be rendered
   return (
-    <LoginOutsideContainer
-    >
-      <LoginGrid
-      >
-        <LoginCard
-        >
+    <LoginOutsideContainer>
+      <LoginGrid>
+        <LoginCard>
           <FormProvider {...methods}>
-            <LoginBorder
-            >
-              <LoginInsideGrid
-              >
-                <Grid
-                  item
-                  xs={12}
-                  sm={6}
-                  sx={{ borderRight: { sm: "1px solid #ddd" } }}
-                >
+            <LoginBorder>
+              <LoginInsideGrid>
+                <LeftBoxContainer>
                   <LeftBox
                     noValidate
                     autoComplete="off"
                     onSubmit={methods.handleSubmit(onSubmitHandler)}
                   >
-                    <Typography
-                      variant="h6"
-                      component="h1"
-                      sx={{ textAlign: "center", mb: "1.5rem" }}
-                    >
-                      Log into your account
-                    </Typography>
+                    <LoginText>Log into your account</LoginText>
 
                     <FormInput
                       label="Enter your email"
@@ -144,16 +102,7 @@ const RightBox = styled.div`
                         />
                       }
                       label={
-                        <Typography
-                          variant="body2"
-                          sx={{
-                            fontSize: "0.8rem",
-                            fontWeight: 400,
-                            color: "#5e5b5d",
-                          }}
-                        >
-                          Trust this device
-                        </Typography>
+                        <TrustDeviceText>Trust this device</TrustDeviceText>
                       }
                     />
 
@@ -171,21 +120,10 @@ const RightBox = styled.div`
                       Login
                     </LoadingButtonMui>
                   </LeftBox>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <Typography
-                    variant="h6"
-                    component="p"
-                    sx={{
-                      paddingLeft: { sm: "3rem" },
-                      mb: "1.5rem",
-                      textAlign: "center",
-                    }}
-                  >
-                    Log in with another provider:
-                  </Typography>
-                  <RightBox
-                  >
+                </LeftBoxContainer>
+                <RightBoxContainer>
+                  <LoginText>Log in with another provider:</LoginText>
+                  <RightBox>
                     <OauthMuiLink href="">
                       <GoogleLogo style={{ height: "2rem" }} />
                       Google
@@ -195,20 +133,18 @@ const RightBox = styled.div`
                       GitHub
                     </OauthMuiLink>
                   </RightBox>
-                </Grid>
+                </RightBoxContainer>
               </LoginInsideGrid>
-              <Grid container justifyContent="center">
-                <Stack sx={{ mt: "3rem", textAlign: "center" }}>
-                  <Typography sx={{ fontSize: "0.9rem", mb: "1rem" }}>
-                    Need an account?{" "}
-                    <LinkItem to="/signup">Sign up here</LinkItem>
-                  </Typography>
-                  <Typography sx={{ fontSize: "0.9rem" }}>
-                    Forgot your{" "}
-                    <LinkItem to="/forgotPassword">password?</LinkItem>
-                  </Typography>
-                </Stack>
-              </Grid>
+              <BottomLoginContainer>
+                <LinkText margin="1rem">
+                  Need an account?{" "}
+                  <LinkItem to="/signup">Sign up here</LinkItem>
+                </LinkText>
+                <LinkText>
+                  Forgot your{" "}
+                  <LinkItem to="/forgotPassword">password?</LinkItem>
+                </LinkText>
+              </BottomLoginContainer>
             </LoginBorder>
           </FormProvider>
         </LoginCard>
---
+++ src/pages/SignIn/style.ts
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+
+
+export const LoginOutsideContainer = styled.div`
+height: 100vh;
+background-color: #f4f4f4;
+`;
+
+export const LoginGrid = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+export const LoginCard = styled.div`
+max-width: 70rem;
+width: 100%;
+background-color: #fff;
+box-shadow: 0 0 5px #ddd;
+`;
+
+export const LoginBorder = styled.div`
+box-shadow: 0 0 5px #ddd;
+padding: 6rem 1rem;
+`;
+
+export const LoginInsideGrid = styled.div`
+  justify-content: space-between;
+  max-width: 100%;
+  margin: 0 auto;
+  @media (min-width: 600px) {
+    display: flex;
+    max-width: 45rem;
+  }
+`;
+
+export const LeftBox = styled.form`
+display: flex;
+flex-direction: column;
+width: 80%;
+margin-inline: auto;
+@media (min-width: 600px) {
+    padding-right: 3rem;
+    width: 100%;
+  }
+`;
+
+export const RightBox = styled.div`
+display: flex;
+flex-direction: column;
+width: 80%;
+margin-inline: auto;
+row-gap: 1rem;
+
+@media (min-width: 600px) {
+    padding-left: 3rem;
+    width: 100%;
+  }
+`;
+
+export const LoginText = styled.h1`
+margin: 0;
+font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+font-weight: 500;
+font-size: 1.25rem;
+line-height: 1.6;
+letter-spacing: 0.0075em;
+text-align: center;
+margin-bottom: 1.5rem;
+`;
+
+export const TrustDeviceText = styled.h1`
+margin: 0;
+font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+line-height: 1.43;
+letter-spacing: 0.01071em;
+color: #5e5b5d;
+font-size: 0.8rem;
+font-weight: 400;
+`;
+
+export const BottomLoginContainer = styled.div`
+display:flex;
+flex-direction:column;
+justify-content:center;
+margin-top: 3rem;
+text-align: center;
+`;
+
+type LinkTextInput = {
+margin ?: string
+}
+
+export const LinkText = styled.h1<LinkTextInput>`
+margin: 0;
+font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+line-height: 1.43;
+letter-spacing: 0.01071em;
+font-size: 0.9rem;
+font-weight: 400;
+margin-bottom: ${ ({margin}) => (margin ? margin : 0) };
+`;
